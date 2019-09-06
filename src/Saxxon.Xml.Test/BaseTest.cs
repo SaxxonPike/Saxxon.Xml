c720c6885a0379d744d932e3790176e31958bb38
SaxxonPike <saxxonpike@gmail.com>
2019-09-06 10:18:44 -0500

    Test Object.IfNotNull and Object.IfNull.

--- src/Saxxon.Xml.Test/BaseTest.cs
+++ src/Saxxon.Xml.Test/BaseTest.cs
@@ -7,33 +7,39 @@ namespace Saxxon.Xml.Test
     public abstract class BaseTest
     {
         [ExcludeFromCodeCoverage]
-        protected static string RandomString()
-        {
-            return TestContext.CurrentContext.Random.GetString();
-        }
+        protected static string RandomString() => 
+            TestContext.CurrentContext.Random.GetString();
 
         [ExcludeFromCodeCoverage]
-        protected static string RandomString(int length)
-        {
-            return TestContext.CurrentContext.Random.GetString(length);
-        }
+        protected static string RandomString(int length) => 
+            TestContext.CurrentContext.Random.GetString(length);
 
         [ExcludeFromCodeCoverage]
-        protected static int RandomInt()
-        {
-            return TestContext.CurrentContext.Random.Next();
-        }
+        protected static int RandomInt() => 
+            TestContext.CurrentContext.Random.Next();
 
         [ExcludeFromCodeCoverage]
-        protected static int RandomInt(int max)
-        {
-            return TestContext.CurrentContext.Random.Next(max);
-        }
+        protected static int RandomInt(int max) => 
+            TestContext.CurrentContext.Random.Next(max);
 
         [ExcludeFromCodeCoverage]
-        protected static int RandomInt(int min, int max)
-        {
-            return TestContext.CurrentContext.Random.Next(min, max);
-        }
+        protected static int RandomInt(int min, int max) => 
+            TestContext.CurrentContext.Random.Next(min, max);
+
+        [ExcludeFromCodeCoverage]
+        protected static void Pass() => 
+            NUnit.Framework.Assert.Pass();
+
+        [ExcludeFromCodeCoverage]
+        protected static void Pass(string reason) => 
+            NUnit.Framework.Assert.Pass(reason);
+        
+        [ExcludeFromCodeCoverage]
+        protected static void Fail() => 
+            NUnit.Framework.Assert.Fail();
+
+        [ExcludeFromCodeCoverage]
+        protected static void Fail(string reason) => 
+            NUnit.Framework.Assert.Fail(reason);
     }
 }
---
+++ src/Saxxon.Xml.Test/FluentCommonTests.Object.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace Saxxon.Xml.Test
+{
+    public abstract partial class FluentCommonTests
+    {
+        [Test]
+        public void Object_IfNull_ShouldExecute_WhenNull()
+        {
+            // Arrange.
+            IFluentXmlObject obj = null;
+            
+            // Act.
+            // ReSharper disable once ExpressionIsAlwaysNull
+            obj.IfNull(Pass);
+            
+            // Assert.
+            Fail("delegate was not called");
+        }
+
+        [Test]
+        public void Object_IfNull_ShouldNotExecute_WhenNotNull()
+        {
+            // Arrange.
+            var obj = new Mock<IFluentXmlObject>().Object;
+            
+            // Act.
+            obj.IfNull(() => Fail("delegate was called"));
+            
+            // Assert.
+            Pass();
+        }
+
+        [Test]
+        public void Object_IfNotNull_ShouldNotExecute_WhenNull()
+        {
+            // Arrange.
+            IFluentXmlObject obj = null;
+            
+            // Act.
+            // ReSharper disable once ExpressionIsAlwaysNull
+            obj.IfNotNull(_ => Fail("delegate was called"));
+            
+            // Assert.
+            Pass();
+        }
+
+        [Test]
+        public void Object_IfNotNull_ShouldExecute_WhenNotNull()
+        {
+            // Arrange.
+            var obj = new Mock<IFluentXmlObject>().Object;
+            
+            // Act.
+            obj.IfNotNull(_ => Pass());
+            
+            // Assert.
+            Fail("delegate was not called");
+        }
+    }
+}
